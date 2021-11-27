3a7413e85705ced4ed31a2f5f87634e2060a8fe2
youssef alaa thabet <youssefalaa858@yahoo.com>
2021-11-27 20:40:45 +0200

    fixing some problems in flight searching and updating select flight

--- client/src/pages/SelectFlight.js
+++ client/src/pages/SelectFlight.js
@@ -8,15 +8,26 @@ import { json } from "body-parser";
 
 
 const SelectFlight = () => {
+    
     const [searchFlights, setSearchFlights] = useContext(UserHomeCtx);
-    let f = searchFlights.data.going;
-    let flights = f;
+    //let f = 
+    let flights =searchFlights.data.going;
     let seatType = searchFlights.data.seatType;
+    console.log("testing",searchFlights.data.going);
+    function GetTime(date1){
+        return new Date(date1).getHours()+":"+new Date(date1).getMinutes();
+    }
+    function GetDate(date1){
+        var date= new Date(date1);
+        var z= date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear();
+       return z; 
+    }
      function getTime(date1,date2){
          var hours2= new Date(date2).getHours();
          var hours1= new Date(date1).getHours();
          var minutes2= new Date(date2).getMinutes();
          var minutes1= new Date(date1).getMinutes();
+         console.log("Day:",new Date(date1).getDate());
          var hours=0;
          var minutes=(minutes2+(60-minutes1));
          if(hours2>hours1){
@@ -26,14 +37,20 @@ const SelectFlight = () => {
              hours=(24-hours1)+hours2;
          }
          while(minutes>60){
-            hours2=hours2+1;
+            hours=hours+1;
             minutes=minutes-60;
+            
          }
-         var duration = hours+":"+minutes;
+         if(minutes==60){
+            hours=hours+1;
+            minutes=minutes-60;
+
+        }
+         var duration = hours+" hours "+minutes+" minutes";
          return duration; 
          }
 
-    console.log("flight here--------------",flights);
+   // console.log("flight here--------------",flights);
     /*const  state  = window.props;
     console.log("----------------------------------------",window.props);
 */
@@ -45,19 +62,22 @@ const SelectFlight = () => {
             <br/>
             <br/>
  
-        <h1> Reservation Summary {JSON.stringify(searchFlights.data.going)}</h1>
-        {flights.map((flight) => (
+         {/* <h1> Reservation Summary {JSON.stringify(searchFlights.data.going)}</h1>  */}
+                 { flights.map((flight) => (
             <div class="card">
             <div class="card-body">
-            <h5 class="card-title">{flight.flightNumber}</h5>
-            <h6>{flight.departureTime}</h6>
-            <h6>{flight.arrivalTime}</h6>
-            <h6>{seatType}</h6>
-            <h6>{getTime("2022-01-21T21:56:00.000Z","2022-01-21T10:13:00.000Z")}</h6> 
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">Flight Number:  {flight.flightNumber}</h5>
+         <h6>Departure Date:  {GetDate(flight.departureTime)}</h6>
+             <h6>Arrival Date:  {GetDate(flight.arrivalTime)} </h6> 
+            <h6>Departure Time:  {GetTime(flight.departureTime)}</h6>
+            <h6>Arrival Time:  {GetTime(flight.arrivalTime)}</h6>
+            <h6>Class:  {seatType}</h6>
+            <h6>Duration: {getTime(flight.departureTime,flight.arrivalTime)}</h6> 
+            <h6>Baggage Allowance:  2 Bags</h6>
+            <a href="#" class="btn btn-primary">Select Flight ✈</a>
             </div>
             </div>
-        ))};
+        ))};  
         <Link to="/UserHome">{"<< Get Back"}</Link>
 
         </div> 
