396b2308072468e938bca6ac3336084253bf22fa
Yousef <yousefelbon@gmail.com>
2021-11-27 20:44:17 +0200

    latest

--- client/src/Components/userHomePage.js
+++ client/src/Components/userHomePage.js
@@ -60,7 +60,7 @@ const UserHomePage = () => {
          // console.log("recived ===> ",data);
           r=data.going;
           s= data.returning;
-          searchFlights.data=data;
+          searchFlights=data.going;
           /*goingData=data.going; 
           this.props.history.push({
             pathname: '/SelectFlight',
@@ -119,28 +119,72 @@ const UserHomePage = () => {
      // action="http://localhost:8080/GetRequestedFlights"
       //method="post"
     >
-      <Carousel />
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            style={{ height: "10cm" }}
-            src="https://media.istockphoto.com/photos/passenger-airplane-flying-above-clouds-during-sunset-picture-id155439315?b=1&k=20&m=155439315&s=170667a&w=0&h=N2BzlH2GYabhWN0LXZTqTkVODuTb8qDAESQYCPzIig8="
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>Above All, We Care</h3>
-            <p></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            style={{ height: "10cm" }}
-            src="https://media.istockphoto.com/photos/corporate-jet-picture-id1305805559?b=1&k=20&m=1305805559&s=170667a&w=0&h=PgS30I7bdCmaWZXC7tHeRjhAUFoiv7LDgeqJBpfVorw="
-            alt="Second slide"
-          />
+      <Row>
+      <h3 className="mt-3 mb-2">Book Your Flight! ✈ </h3>
 
-      </Carousel.Item>
+
+      <Col >
+        <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>From <FontAwesomeIcon icon={faPlaneDeparture} /></Form.Label>
+      <Form.Select  name="departureAirport" defaultValue="">
+        <option value="LAX">LAX</option>
+        <option  value="JFK">JFK</option>
+        <option value="LHR">LHR</option>
+        <option  value="CAI">CAI </option>
+        <option value="EXP">EXP</option>
+        <option value="MUC">MUC</option>
+        <option value="CDG">CDG</option>
+        <option value="RUH">RUH</option>
+        <option value="YYZ">YYZ</option>
+        <option value="FRA">FRA</option>
+      </Form.Select>
+    </Form.Group>
+        </Col>
+
+      <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>To <FontAwesomeIcon icon={faPlaneArrival} /></Form.Label>
+      
+      <Form.Select  name="arrivalAirport" defaultValue="">
+        <option   value="LAX">LAX</option>
+        <option  value="JFK">JFK</option>
+        <option value="LHR">LHR</option>
+        <option  value="CAI"> CAI </option>
+        <option value="EXP">EXP</option>
+        <option value="MUC">MUC</option>
+        <option value="CDG">CDG</option>
+        <option value="RUH">RUH</option>
+        <option value="YYZ">YYZ</option>
+        <option value="FRA">FRA</option>
+      </Form.Select>
+    </Form.Group>
+       
+      </Row>
+      <Row>
+      <Col>
+      <Form.Group  style={{width:"auto"}}  controlId="formGridDepartureTime">
+          <Form.Label>Departure Date  <FontAwesomeIcon icon={faCalendarAlt} /></Form.Label>
+          <Form.Control
+            type="datetime-local"
+            name="departureTime"
+            placeholder="Enter Departure Time"
+          />
+        </Form.Group>
+        
+        </Col>
+        <Col>
+          
+        <Form.Group  style={{width:"auto"}}  controlId="formGridArrivalTime">
+          <Form.Label>Arrival Date <FontAwesomeIcon icon={faCalendarAlt} /></Form.Label>
+          <Form.Control
+            type="datetime-local"
+            name="arrivalTime"
+            placeholder="Enter Arrival Time"
+          />
+        </Form.Group>
+        </Col>
+       
+
+      </Row>
 
       <Row >
       <Form.Group as={Col} controlId="formGridState">
@@ -187,8 +231,8 @@ const UserHomePage = () => {
     </Col>
     <Col>
 
-    <Link to= "/SelectFlight"> 
-    <Button  className="mt-4"  variant="primary" onClick={handleSubmit} >
+    <Link to= "/SelectFlight"
+> <Button  className="mt-4"  variant="primary" onClick={handleSubmit} >
             Search Flight  <FontAwesomeIcon icon={faSearch} />   
           </Button>
           </Link>
@@ -210,8 +254,6 @@ const UserHomePage = () => {
 
 
     ); 
-
-      {/* <img style={{marginLeft:"8cm"}} src="https://as2.ftcdn.net/v2/jpg/00/79/10/15/1000_F_79101584_S9c81T4XBEn6wOJCYRFwWkqUpgmi05vJ.jpg" /> */}
 
 };
 export default UserHomePage;
